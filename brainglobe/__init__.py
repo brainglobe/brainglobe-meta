aeb352e44e5d91eec1a752d5f7d5f07bfb7f8c97
willGraham01 <1willgraham@gmail.com>
2023-06-21 09:54:21 +0100

    Make morphapi and cellfinder optional, adjust tests

--- brainglobe/__init__.py
+++ brainglobe/__init__.py
@@ -12,4 +12,18 @@ import bg_space
 import brainreg
 import brainreg_segment
 import cellfinder_core
-import morphapi
+
+# Determine if optional dependencies were installed,
+# and expose if necessary.
+# morphapi
+_MORPHAPI_INSTALLED = True
+try:
+    import morphapi
+except ImportError:
+    _MORPHAPI_INSTALLED = False
+# cellfinder - not exposed, but still check
+_CELLFINDER_INSTALLED = True
+try:
+    import cellfinder
+except ImportError:
+    _CELLFINDER_INSTALLED = False
--- tests/test_unit/test_tool_exposure.py
+++ tests/test_unit/test_tool_exposure.py
@@ -9,8 +9,8 @@ EXPOSED_TOOLS = [
     "brainreg",
     "brainreg_segment",
     "cellfinder_core",
-    "morphapi",
 ]
+OPTIONAL_TOOLS = ["morphapi", "cellfinder"]
 
 
 def test_tool_exposure() -> None:
@@ -23,3 +23,28 @@ def test_tool_exposure() -> None:
         assert inspect.ismodule(
             getattr(bg, exposed_tool)
         ), f"brainglobe.{exposed_tool} is not a submodule"
+
+    # Now check the optional dependencies
+    # morphapi
+    bg_has_morphapi = hasattr(bg, "morphapi")
+    if bg._MORPHAPI_INSTALLED:
+        assert (
+            bg_has_morphapi
+        ), "brainglobe has morphapi, but it is flagged as installed"
+        assert inspect.ismodule(
+            getattr(bg, "morphapi")
+        ), "brainglobe.morphapi is not a submodule"
+    else:
+        assert (
+            not bg_has_morphapi
+        ), "brainglobe has morphapi, but it is flagged as not installed"
+    # cellfinder
+    bg_has_cellfinder = hasattr(bg, "cellfinder")
+    if bg._CELLFINDER_INSTALLED:
+        assert (
+            bg_has_cellfinder
+        ), "brainglobe has cellfinder, but it is flagged as installed"
+    else:
+        assert (
+            not bg_has_cellfinder
+        ), "brainglobe has cellfinder, but it is flagged as not installed"
