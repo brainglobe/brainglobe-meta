8b5112acb436a568dbde4c59a72c8133e611bd80
Will Graham <32364977+willGraham01@users.noreply.github.com>
2024-01-03 10:29:39 +0000

    Update packages that are shipped (#37)
    * Ship brainrender. Remove conda-checks that are currently not used, as we're not on conda
    
    * Add brainrender-napari
    
    * Remove morphapi

--- brainglobe/__init__.py
+++ brainglobe/__init__.py
@@ -1,25 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
-from ._conda_handles import _CELLFINDER_INSTALLED, _MORPHAPI_INSTALLED
-
 try:
     __version__ = version("brainglobe")
-except PackageNotFoundError:
+except PackageNotFoundError as e:
     # package is not installed
     pass
-
-# Expose tools under the brainglobe namespace
-import bg_atlasapi
-import bg_space
-import brainreg
-import brainglobe_segmentation
-
-# Expose tools that may not be present
-# if a conda install was performed
-if _MORPHAPI_INSTALLED:
-    # Import morphapi
-    import morphapi
-
-# cellfinder and associated packages
-if _CELLFINDER_INSTALLED:
-    import cellfinder_core
