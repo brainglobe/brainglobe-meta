e571a672b447cbd8acf43c725774ba5bc72c9ea4
willGraham01 <1willgraham@gmail.com>
2024-01-25 16:05:39 +0000

    Add first instance of bibtex converter class

---
+++ brainglobe/citation/bibtex_fmt.py
@@ -0,0 +1,192 @@
+import warnings
+from string import ascii_letters, digits
+from typing import ClassVar, Dict, List
+
+
+class BibTexEntry:
+    """
+    An abstract base class for generating BibTex entries from
+    CITATION.cff yaml-content.
+
+    Constructed by passing a dict containing the yaml-processed
+    content of the CITATION.cff file to the constructor.
+    Required fields are checked for on instantiation.
+    Missing optional fields will be set to None if not provided.
+
+    Parameters
+    ----------
+    information : Dict[str, str]
+        yaml-processed content of CITATION.cff.
+    cite_key : str, default = "BrainGlobeReference"
+        Citation key to add to the BibTex reference that is generated.
+    suppress_unused: bool, default = True
+        If True, warn the user about the fields in the information
+        input that are not required nor optional, so are ignored.
+
+    Attributes
+    ----------
+    cite_key : str
+        The citation key that appears in the BibTex reference
+    entry_type : ClassVar[str]
+        The BibTex entry type; article, book, software, etc
+    required : ClassVar[List[str]]
+        Required fields for the entry to be created
+    optional : ClassVar[List[str]]
+        Optional fields that the entry may possess
+    indent_character : str, default = ' ' * 4
+        Character sequence to use for indentation.
+    """
+
+    # The citation key that appears in the BibTex reference
+    cite_key: str
+    # The BibTex entry type; article, book, software, etc
+    entry_type: ClassVar[str]
+    # Required fields for the entry to be created
+    required: ClassVar[List[str]]
+    # Optional fields that the entry may possess
+    optional: ClassVar[List[str]]
+
+    # Character sequence to use for indentation
+    indent_character: str = " " * 4
+
+    # mypy type-hints
+    author: str | Dict[str, str] | List[Dict[str, str]]
+
+    @classmethod
+    def validate_citation_key(cls, key: str) -> bool:
+        """
+        Return True if the citation key provided is valid for use,
+        otherwise return False.
+
+        Valid citation keys may contain;
+        - Alphanumeric characters,
+        - Underscores (_),
+        - Single dashes (-),
+        - Semicolons (:),
+
+        and no others.
+        """
+        bad_characters = key.lower()
+        for char in ascii_letters + digits + "_-:":
+            bad_characters = bad_characters.replace(char, "")
+
+        if bad_characters:
+            # Some characters in the string provided are not permitted,
+            # as we have removed all of the permitted characters.
+            return False
+        return True
+
+    def __init__(
+        self,
+        information: Dict[str, str],
+        cite_key: str = "BrainGlobeReference",
+        suppress_unused: bool = True,
+    ) -> None:
+        """ """
+        # Add the citation key if provided,
+        # or use the default otherwise
+        if self.validate_citation_key(cite_key):
+            self.cite_key = cite_key
+
+        # Add all the information we need
+        for key, value in information.items():
+            if key in self.required:
+                self.key = value
+            elif key in self.optional:
+                self.key = value
+            elif suppress_unused:
+                warnings.warn(
+                    f"Not sure what to do with {key} for entry of type "
+                    f"{self.cite_key}"
+                )
+
+        # Check that all required information is populated
+        for required_field in self.required:
+            if not hasattr(self, required_field):
+                raise KeyError(
+                    f"Entry of type {self.cite_key} did not receive value"
+                    f" for required key: {required_field}"
+                )
+        # Optional fields should be set to None so that checks against
+        # them produce nothing and evaluated to False
+        for optional_field in self.optional:
+            if not hasattr(self, optional_field):
+                self.optional_field = None
+
+        # If we have an author field, we will need to parse the
+        # dictionary input into the string that BibTex is expecting
+        if hasattr(self, "author"):
+            self._prepare_author_field()
+
+        return
+
+    def _prepare_author_field(self) -> None:
+        """
+        The authors field may come in as a dict, or a list of dicts.
+        This is rather inconvenient as we need it to be a string, so
+        convert it with this function.
+
+        author fields in CITATION.cff are:
+        - family-names
+        - given-names
+        - orcid
+        - affiliation
+
+        or which we only need the names.
+        """
+        # A single author will be read in as a dictionary
+        if isinstance(self.author, dict):
+            surname = self.author["family-names"]
+            forename = self.author["given-names"]
+            self.author = f"{forename} {surname}"
+        # Multiple authors will be read in as a list of dictionaries
+        elif isinstance(self.author, list):
+            all_authors = []
+            for author_info in self.author:
+                if not isinstance(author_info, dict):
+                    raise ValueError(
+                        f"Expected author to be dictionary but it was"
+                        f" {type(author_info)}: {author_info}"
+                    )
+                else:
+                    surname = author_info["family-names"]
+                    forename = author_info["given-names"]
+                    all_authors.append(f"{forename} {surname}")
+            self.author = "and".join(all_authors)
+        # Unrecognised read format, abort
+        else:
+            raise ValueError(
+                f"Expected authors to be either dict or list of dicts,"
+                f" not {type(self.author)}"
+            )
+        return
+
+    def write(self) -> str:
+        """
+        Write the appropriate reference text,
+        using the populated fields.
+        """
+        output_string = f"@{self.entry_type}{{{self.cite_key},"
+
+        # Tracks current indentation level
+        indent_level = 1
+
+        # Required fields are guaranteed to exist
+        for req_field in self.required:
+            output_string += (
+                self.indent_character * indent_level
+                + f'{req_field} = "{getattr(self, req_field)}",\n'
+            )
+
+        # Optional fields may be skipped
+        for opt_field in self.optional:
+            if getattr(self, opt_field):
+                output_string += (
+                    self.indent_character * indent_level
+                    + f'{opt_field} = "{getattr(self, opt_field)}",\n'
+                )
+
+        indent_level -= 1
+        output_string += "}"
+
+        return output_string
